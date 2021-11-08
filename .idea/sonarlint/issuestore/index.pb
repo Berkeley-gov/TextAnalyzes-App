3e967a84d1987f1b5f7b89c6b6037268bf1c4bd3
Alchemist <59194756+Berkeley-gov@users.noreply.github.com>
2021-11-07 18:25:43 -0800

    Added as much anlysis features as I can

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+C
+text-app/src/App.js,c/1/c127d97e288a918a6da0c4956cffa477621b1e1f
+E
+text-app/src/index.js,6/9/6907c532a89eb3304c15420e2d6942396a75788d
+Q
+!text-app/src/components/Navbar.js,5/4/5408fff39a74306ef8431870edf7e01d4e3c2117
+[
++text-app/src/components/TextAnalysisForm.js,2/f/2f9afd8bf86cf6dbad9c0ee9075f1f309ef3712f
+J
+text-app/public/index.html,0/0/003205ae37d2cf90157cc3e8db7e0c97f15baaf5
--- text-app/src/components/TextAnalysisForm.js
+++ text-app/src/components/TextAnalysisForm.js
@@ -25,36 +25,91 @@ export default function TextAnalysisForm() {
      * used for the user input.
      * @param data
      */
-    const analyzeKeywordsFrequency = (data) => {
-        console.log(`data from api: ${data} \nType of data type: ${Array.isArray(data)}`);
+    const analyzeKeywordsFrequency = () => {
+        let filteredArray = [];
 
         // Converting the incoming user input as a string to an array of strings for comparison.
         const inputArray = inputText.split(' ');
-
-        console.log(`user input: ${inputArray} ${Array.isArray(inputArray)}`);
 
         // Nested for loop is used to check for any common english word matches and their frequency
         for(let i = 0; i < inputArray.length; i++) {
             for(let j = 0; j < commonWords.length; j++) {
                 if(inputArray[i] === commonWords[j]) {
-                    console.log(`It's a match: ${commonWords[j]}`);
+                    filteredArray.push(inputArray[i])
                 }
             }
         }
 
+        return filteredArray;
+    }
+
+    // Word counting function that determines keyword frequency.
+    const analyze1000CommonWords = (array) => {
+        let counts = {};
+        let keys = [];
+
+        for(let i = 0; i < array.length; i++) {
+            let word = array[i].toLowerCase();
+
+            if(!/\d+/.test(word)) {
+                if(counts[word] === undefined) {
+                    counts[word] = 1;
+                    keys.push(word);
+                } else {
+                    counts[word] = counts[word] + 1;
+                }
+            }
+        }
+
+        keys.sort(compare);
+
+        function compare(a, b) {
+            let countA = counts[a];
+            let countB = counts[b];
+
+            return countB - countA;
+        }
+
+        return counts;
+    }
+
+    const createKeyFrequencyUI = (array) => {
+        console.log(`Final array ui function is working:`);
+        console.log(array)
+
+        // keyWords holds a list of the top keywords found in the user's input.
+        const dataValues = Object.entries(array);
+
+        // Creating the div element that holds the list of keywords for UI.
+        let wordCard = document.getElementById("words");
+        wordCard.classList.add("card", "p-1", "m-5", "shadow-sm", "bg-light");
+
+        let testString = reversingString(inputText);
+        let reversedString = reverseUserInput(inputText);
+
+        wordCard.innerHTML = `<p><strong>The User had inputted the following:</strong> ${inputText}</p>
+                               <br/>
+                               <p><strong>Input reversed by word:</strong> ${reversedString}</p>
+                               <br/>
+                               <p><strong>Input reversed charaters:</strong> ${testString}</p>
+                               <br/>
+                               <p><strong>Most common used words:</strong> ${dataValues}</p>`;
     }
 
     // Handles the state of the text input coming in from the text area field.
     const handleTextFieldSubmit = (event) => {
         event.preventDefault();
 
-        analyzeKeywordsFrequency(commonWords);
+        // Array is filtered to root out the usage for any of the 1000 common english words.
+        let analyzedArray =  analyzeKeywordsFrequency();
+        const completedArray = analyze1000CommonWords(analyzedArray);
+
+        console.log(completedArray)
+        console.log('filtering is completed.');
+
+        createKeyFrequencyUI(completedArray);
 
         // Testing methods here ++
-        let testString = reversingString(inputText);
-
-        let reversedString = reverseUserInput(inputText);
-        window.alert(`User had inputted the following: ${reversedString} \n\nTest 1: ${testString}`);
     }
 
 
@@ -83,6 +138,10 @@ export default function TextAnalysisForm() {
     // JSX for the GUI display the form to the client; form gathers input and stores it in the controlled hook state for further analyzes.
     return(
         <main className="container p-5">
+            <div id="words" style={{marginBottom: 60}}>
+
+            </div>
+
             <form style={{ borderColor: '#0576c1' }} className="card row shadow-lg rounded bg-light" onSubmit={handleTextFieldSubmit}>
                 <h2 className="card-header bg-dark text-light text-center p-3 m-0">Analyze Your Text</h2>
 
@@ -102,8 +161,6 @@ export default function TextAnalysisForm() {
                     </div>
                 </div>
             </form>
-
-            <div id="test-display" className="container card mt-1"></div>
         </main>
     );
 }
